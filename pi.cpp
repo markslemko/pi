f1afb66fdc96dd9ab45a1126e4ddc9d22227d8cb
Mark Slemko <markslemko@gmail.com>
2020-05-17 00:01:13 -0700

    refactor output to be cleaner/simpler and add a few more decimal places to calculate

--- pi.cpp
+++ pi.cpp
@@ -6,43 +6,39 @@ namespace boost {
         typedef number<cpp_bin_float<150>> cpp_bin_float_150;
         typedef number<cpp_bin_float<200>> cpp_bin_float_200;
         typedef number<cpp_bin_float<250>> cpp_bin_float_250;
+        typedef number<cpp_bin_float<500>> cpp_bin_float_500;
+        typedef number<cpp_bin_float<1000>> cpp_bin_float_1000;
+        typedef number<cpp_bin_float<2000>> cpp_bin_float_2000;
+        typedef number<cpp_bin_float<5000>> cpp_bin_float_5000;
     }
 }
 
-//#define PI_ITERATIVE
+#define PI_ITERATIVE
 #include "pi.h"
+
+template<typename T>
+void outputPi(std::string myTypeName) {
+    std::cout.precision(std::numeric_limits<int>::max_digits10);
+    std::cout << "<" << myTypeName << ">:\n" << pi<T>() << "\n\n" << std::flush;
+}
 
 int main()
 {
     std::cout << "PI:\n\n" << std::flush;
 
-    std::cout.precision(std::numeric_limits<int>::max_digits10);
-    std::cout << "<long long>:\n" << pi<long long>() << "\n\n" << std::flush;
-
-    std::cout.precision(std::numeric_limits<float>::max_digits10);
-    std::cout << "<float>:\n" << pi<float>() << "\n\n" << std::flush;
-
-    std::cout.precision(std::numeric_limits<double>::max_digits10);
-    std::cout << "<double>:\n" << pi<double>() << "\n\n" << std::flush;
-
-    std::cout.precision(std::numeric_limits<long double>::max_digits10);
-    std::cout << "<long double>:\n" << pi<long double>() << "\n\n" << std::flush;
-
-    std::cout.precision(std::numeric_limits<boost::multiprecision::cpp_bin_float_double_extended>::max_digits10);
-    std::cout << "<boost::multiprecision::cpp_bin_float_double_extended>:\n" << pi<boost::multiprecision::cpp_bin_float_double_extended>() << "\n\n" << std::flush;
-
-    std::cout.precision(std::numeric_limits<boost::multiprecision::cpp_bin_float_50>::max_digits10);
-    std::cout << "<boost::multiprecision::cpp_bin_float_50>:\n" << pi<boost::multiprecision::cpp_bin_float_50>() << "\n\n" << std::flush;
-
-    std::cout.precision(std::numeric_limits<boost::multiprecision::cpp_bin_float_100>::max_digits10);
-    std::cout << "<boost::multiprecision::cpp_bin_float_100>:\n" << pi<boost::multiprecision::cpp_bin_float_100>() << "\n\n" << std::flush;
-
-    std::cout.precision(std::numeric_limits<boost::multiprecision::cpp_bin_float_150>::max_digits10);
-    std::cout << "<boost::multiprecision::cpp_bin_float_150>:\n" << pi<boost::multiprecision::cpp_bin_float_150>() << "\n\n" << std::flush;
-
-    std::cout.precision(std::numeric_limits<boost::multiprecision::cpp_bin_float_200>::max_digits10);
-    std::cout << "<boost::multiprecision::cpp_bin_float_200>:\n" << pi<boost::multiprecision::cpp_bin_float_200>() << "\n\n" << std::flush;
-
-    std::cout.precision(std::numeric_limits<boost::multiprecision::cpp_bin_float_250>::max_digits10);
-    std::cout << "<boost::multiprecision::cpp_bin_float_250>:\n" << pi<boost::multiprecision::cpp_bin_float_250>() << "\n\n" << std::flush;
+    outputPi<long long>("long long");
+    outputPi<float>("float");
+    outputPi<double>("double");
+    outputPi<long double>("long double");
+    outputPi<boost::multiprecision::cpp_bin_float_double_extended>("boost::multiprecision::cpp_bin_float_double_extended");
+    outputPi<boost::multiprecision::cpp_bin_float_50>("boost::multiprecision::cpp_bin_float_50");
+    outputPi<boost::multiprecision::cpp_bin_float_100>("boost::multiprecision::cpp_bin_float_100");
+    outputPi<boost::multiprecision::cpp_bin_float_150>("boost::multiprecision::cpp_bin_float_150");
+    outputPi<boost::multiprecision::cpp_bin_float_200>("boost::multiprecision::cpp_bin_float_200");
+    outputPi<boost::multiprecision::cpp_bin_float_250>("boost::multiprecision::cpp_bin_float_250");
+    outputPi<boost::multiprecision::cpp_bin_float_500>("boost::multiprecision::cpp_bin_float_500");
+    outputPi<boost::multiprecision::cpp_bin_float_1000>("boost::multiprecision::cpp_bin_float_1000");
+    outputPi<boost::multiprecision::cpp_bin_float_2000>("boost::multiprecision::cpp_bin_float_2000");
+    outputPi<boost::multiprecision::cpp_bin_float_5000>("boost::multiprecision::cpp_bin_float_5000");
 }
+
